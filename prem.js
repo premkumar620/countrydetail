fb7cc718898dd18586e8b6b85eca2ec6aff579fe
premkumar620 <139530113+premkumar620@users.noreply.github.com>
2024-07-29 23:29:52 +0530

    prem

--- prem.js
+++ prem.js
@@ -5,7 +5,11 @@ div.innerHTML = 'prem kumar';
 const section = document.createElement('section');
 
 // Append the div to the section
-section.appendChild(div);
+div.appendChild(section);
 
 // Append the section to the body (or any other container element)
-document.body.appendChild(section);
+// document.body.appendChild(section);
+const img = document.createElement('img');
+img.append('prem.jpge')
+section.appendChild(img);
+
